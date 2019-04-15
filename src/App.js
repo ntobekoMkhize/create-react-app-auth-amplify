368fbcb959c610a5e0d1d55aaf712deab29d8d77
ntobekoMkhize <ntobekoMkhize@users.noreply.github.com>
2019-04-15 16:59:29 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            React Tuts
           </a>
         </header>
       </div>
